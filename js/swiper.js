60b4bdfcada05716c42df7f7ea38e84856dd657e
Никита Резниченко <nichita.reznicenco@isa.utm.md>
2023-09-20 22:43:39 +0300

    About us implemented

---
+++ js/swiper.js
@@ -0,0 +1,58 @@
+// const swiper = new Swiper('.swiper', {
+//   // Optional parameters
+//   direction: 'vertical',
+//   loop: true,
+
+//   // If we need pagination
+//   pagination: {
+//     el: '.swiper-pagination',
+//   },
+
+//   // Navigation arrows
+//   // navigation: {
+//   //   nextEl: '.swiper-button-next',
+//   //   prevEl: '.swiper-button-prev',
+//   // },
+
+//   // And if we need scrollbar
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//   },
+// });
+
+
+// const swiperHorizontal = new Swiper('.swiperHorizontal', {
+//   // Optional parameters
+//   direction: 'horizontal',
+//   loop: true,
+
+//   // If we need pagination
+//   pagination: {
+//     el: '.swiperH-pagination',
+//   },
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+
+//   // And if we need scrollbar
+//   // scrollbar: {
+//   //   el: '.swiper-scrollbar',
+//   // },
+// });
+
+
+var swiperH = new Swiper('.swiper-container-h', {
+  pagination: '.swiper-pagination-h',
+  paginationClickable: true,
+  spaceBetween: 50
+});
+var swiperV = new Swiper('.swiper-container-v', {
+  pagination: '.swiper-pagination-v',
+  paginationClickable: true,
+  direction: 'vertical',
+  spaceBetween: 50,
+  nested: true
+});
