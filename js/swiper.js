2b8b2ee47f953006cff56b9e0c45d8080587936b
DmitriiBartash <98520845+DmitriiBartash@users.noreply.github.com>
2023-12-18 21:15:20 +0200

    Swiper

---
+++ js/swiper.js
@@ -0,0 +1,40 @@
+// console.log(55)
+$('.firstSwiper').slick({
+    infinite: true,
+    speed: 500,
+    dots: true,
+    fade: true,
+    cssEase: 'linear',
+    // prevArrow: '<button type="button" class="first-slick-prev"></button>',
+    // nextArrow: '<button type="button" class="first-slick-next"></button>',
+  
+});
+
+$('.secondSwiper').slick({
+    centerMode: true,
+    infinite: true,
+    slidesToShow: 5,
+    // slidesToScroll: 5,
+    // arrows: false,
+    centerPadding: '0',
+    prevArrow: '<button type="button" class="second-slick-prev"></button>',
+    nextArrow: '<button type="button" class="second-slick-next"></button>',
+    autoplay: false,
+    autoplaySpeed: 2000,
+
+    responsive: [
+        {
+            breakpoint: 1100,
+            settings: {
+                slidesToShow: 3
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 5
+            }
+        }
+        
+    ]
+})
