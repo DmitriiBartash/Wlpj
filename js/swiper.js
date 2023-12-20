7856ecb2d53c98fc5ffe42166316acf8525b30f3
DmitriiBartash <98520845+DmitriiBartash@users.noreply.github.com>
2023-12-20 19:44:17 +0200

    Big big commit

--- js/swiper.js
+++ js/swiper.js
@@ -1,70 +1,42 @@
-// const swiper = new Swiper('.swiper', {
-//   // Optional parameters
-//   direction: 'vertical',
-//   loop: true,
+// console.log(55)
+$('.firstSwiper').slick({
+    infinite: true,
+    speed: 500,
+    dots: true,
+    fade: true,
+    cssEase: 'linear',
+    arrows: false,
+    // prevArrow: '<button type="button" class="first-slick-prev"></button>',
+    // nextArrow: '<button type="button" class="first-slick-next"></button>',
+  
+});
 
-//   // If we need pagination
-//   pagination: {
-//     el: '.swiper-pagination',
-//   },
+$('.secondSwiper').slick({
+    centerMode: true,
+    infinite: true,
+    slidesToShow: 5,
+    // slidesToScroll: 5,
+    // arrows: false,
+    centerPadding: '0',
+    prevArrow: '<button type="button" class="second-slick-prev"></button>',
+    nextArrow: '<button type="button" class="second-slick-next"></button>',
+    autoplay: false,
+    autoplaySpeed: 2000,
 
-//   // Navigation arrows
-//   // navigation: {
-//   //   nextEl: '.swiper-button-next',
-//   //   prevEl: '.swiper-button-prev',
-//   // },
-
-//   // And if we need scrollbar
-//   scrollbar: {
-//     el: '.swiper-scrollbar',
-//   },
-// });
-
-
-// const swiperHorizontal = new Swiper('.swiperHorizontal', {
-//   // Optional parameters
-//   direction: 'horizontal',
-//   loop: true,
-
-//   // If we need pagination
-//   pagination: {
-//     el: '.swiperH-pagination',
-//   },
-
-//   // Navigation arrows
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//   },
-
-//   // And if we need scrollbar
-//   // scrollbar: {
-//   //   el: '.swiper-scrollbar',
-//   // },
-// });
-
-
-// var swiperH = new Swiper('.swiper-container-h', {
-//   pagination: '.swiper-pagination-h',
-//   paginationClickable: true,
-//   spaceBetween: 50
-// });
-// var swiperV = new Swiper('.swiper-container-v', {
-//   pagination: '.swiper-pagination-v',
-//   paginationClickable: true,
-//   direction: 'vertical',
-//   spaceBetween: 50,
-//   nested: true
-// });
-
-// var swiper = new Swiper(".mySwiper", {
-//   pagination: {
-//     el: ".swiper-pagination",
-//     type: "progressbar"
-//   },
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   }
-// });
+    responsive: [
+        {
+            breakpoint: 1100,
+            settings: {
+                slidesToShow: 3
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 5
+            }
+        }
+        
+    ]
+})
 
