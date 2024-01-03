aa9a9fd29c77dc96204e1de65083c63d817b48a6
Rtiscev <alexandru.rtiscev@isa.utm.md>
2024-01-03 15:00:29 +0200

    Change callback model (not rdy)

--- Models/CallBackModel.cs
+++ Models/CallBackModel.cs
@@ -10,5 +10,8 @@ namespace LandingPage.Models
 		public string NameSurname { get; set; }
 		[Required]
 		public string PhoneNumber { get; set; }
+		[DataType(DataType.DateTime)]
+		[Required]
+		public DateTime CreationDate { get; set; }
 	}
 }
